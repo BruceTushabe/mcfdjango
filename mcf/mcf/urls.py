ec92c6fbd40f9525b8867a17efb75522ab2c6af0
Bruce Tushabe <brucetushabe@gmail.com>
2024-03-05 08:01:42 +0300

    More functionalities

--- mcf/mcf/urls.py
+++ mcf/mcf/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index , contact, upload, generate, populate_excel_view, success_view
+from core.views import index, contact, upload, generate, populate_excel_view, success_view
 
-APP_NAME = 'core'
+app_name = 'core'
 
 urlpatterns = [
-    #path('', include('core.urls')),
-    path('', index, name='index'),
+    #path('core/', include('core.urls', namespace='core')),
     path('admin/', admin.site.urls),
-    path('contact/', contact, name='contact' ),
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
     path('upload/', upload, name='upload'),
-    path('generate/', generate, name='generate'),
     path('loan_form/', populate_excel_view, name='populate_excel'),
     path('success/', success_view, name='success'),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path('generate/', generate, name='generate'),
+
+    path("__debug__/", include("debug_toolbar.urls")),  # Include Django Debug Toolbar URL pattern
 ]
