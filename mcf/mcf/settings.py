c2d8aeeb5b87f175a36aa8afe346dd3faf662408
syny-pulse <sinaijose002@gmail.com>
2024-03-11 13:33:51 +0300

    fully working database

--- mcf/mcf/settings.py
+++ mcf/mcf/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yawdefault',
+        'NAME': 'mcfyaw',
         'USER': 'postgres',
         'PASSWORD': 'Byaruhanga12',
         'HOST':'localhost',
